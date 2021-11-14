d364347818df1a10a24813fd56f8fc08213fd3e5
Mohamed A. Ibrahim <rageali12@gmail.com>
2021-11-14 09:52:19 +0300

    added js file

--- main.js
+++ main.js
@@ -1,44 +1,59 @@
-const sideBar = document.querySelector('.side-bar')
-const close = document.querySelector('.fa-times')
-const hamburger = document.querySelector('.fa-bars').addEventListener('click', ()=>{
+ const sideBar = document.querySelector('.side-bar')
+ const wrapper = document.querySelector('.wrapper')
+ const close = document.querySelector('.fa-times')
+ const hamburger = document.querySelector('.fa-bars')
+ const form = document.querySelector('.form');
+ const year = document.querySelector('#year')
+ // output
+ const akanName = document.querySelector('.your-akan-name');
+ // checkbox
+ const checkboxMale = document.querySelector('.form-check-input-male')
+ const checkboxFemale = document.querySelector('.form-check-input-female')
+
+ DayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
+
+  let female = [
+     "Adwoa",
+     "Akosua", 
+      "Abenaa", 
+      "Akua",
+      "Yaa",
+      "Afua",
+      "Ama"
+      ];
+  let male = [
+    "Kwasi", 
+    "Kudwo",
+    "Kwabena", 
+    "Kwaku",
+    "Yaw", 
+    "Kofi", 
+    "Kwame"
+  ]
+  
+
   // open
-  if (sideBar.style.display === "none") {
-    sideBar.style.display = "block";
-    
-  } else {
-    sideBar.style.display = "none";
-  }
-  
-  
-})
-// close
-close.addEventListener('click', ()=>{
-  if (sideBar.style.display === "block") {
-    sideBar.style.display = "none";
-    
-  } else {
-    sideBar.style.display = "block";
-  }
-  
-})
+ hamburger.addEventListener('mouseenter', ()=>{
+   if (sideBar.style.display === "none") {
+     sideBar.style.display = "block";
+     
+   } else {
+     sideBar.style.display = "none";
+   }
+ })
+ /**********************************************/
 
-// WORKING 
-// Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-// where;
-//  CC - is the century digits. For example 1989 has CC = 19
+ // close
+ close.addEventListener('click', ()=>{
+   if (sideBar.style.display === "block") {
+     sideBar.style.display = "none";
+     
+   } else {
+     sideBar.style.display = "block";
+   }
+ })
 
-//  YY - is the Year digits (1989 has YY = 89)
 
-//  MM -  is the Month
-
-//  DD - is the Day of the month 
-
-//  mod - is the modulus function ( % )
-
-// let century;
-// let yearLastTwodigit;
-// let month;
-// let Day;
-
-const dayOfWeek = [century, yearLastTwodigit, month, Day]
+ /**********************************************/
 
