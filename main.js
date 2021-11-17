25dabe3ef3e28c01ad4f6f4be9407edcbb17dbe9
Mohamed A. Ibrahim <rageali12@gmail.com>
2021-11-17 08:28:23 +0300

    updated js file

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
  const hamburger = document.querySelector('.fa-bars')
  const form = document.querySelector('.form');
 
- const year = document.querySelector('#year')
+ const year = document.querySelector('#year');
+
  const month = document.querySelector('#month');
  const Day = document.querySelector('#day');
 
@@ -12,54 +13,60 @@
  // output
  const akanName = document.querySelector('.your-akan-name');
  // checkbox
- const checkboxMale = document.querySelector('.form-check-input-male').value;
- const checkboxFemale = document.querySelector('.form-check-input-female').value
 
+
+  /****************MONTH******************************/
 month.addEventListener('change', ()=>{
-  if (month.value <= 0){
+  if (month.value > 12 ||month.value <= 0  ){
     month.style.borderColor ="red"
     month.style.borderWidth ="2" + "px"
+    month.style.backgroundColor ="#d8f3dc"
   }
   else{
     month.style.borderColor = "green"
-
+    month.style.backgroundColor ="#d8f3dc"
   }
 })
-
+  /****************YEAR******************************/
 year.addEventListener('change', ()=>{
   if (year.value <= 1600){
     year.style.borderColor ="red"
     year.style.borderWidth ="2" + "px"
+    year.style.backgroundColor ="#d06267"
   }
   else{
     year.style.borderColor = "green"
-
+    year.style.backgroundColor ="#d8f3dc"
   }
 })
-
+  /****************DAY******************************/
 Day.addEventListener('change', ()=>{
   if (Day.value > 31){
+    Day.style.backgroundColor ="#d06267"
     Day.style.borderColor ="red"
     Day.style.borderWidth ="2" + "px"
   }
   else{
-    day.style.borderColor = "green"
-
+    Day.style.backgroundColor ="#d8f3dc"
+    Day.style.borderColor = "green"
+    Day.style.borderWidth ="2" + "px"
   }
 })
 
 
+    /***************OPEN*******************************/
   // open
   hamburger.addEventListener('click', ()=>{
     if (sideBar.style.display === "none") {
-      sideBar.style.display = "block";
+      sideBar.style.display ="block";
       
     } else {
       sideBar.style.display = "none";
     }
-    form.style.display="block";
+    // form.style.display="block";
+
   })
-  /**********************************************/
+  /******************CLOSE****************************/
  
   // close
   close.addEventListener('click', ()=>{
@@ -69,120 +76,50 @@ Day.addEventListener('change', ()=>{
     } else {
       sideBar.style.display = "block";
     }
-    form.style.display="block";
+    // form.style.display="block";
+  })
+
+  /***************SUBMIT TO RUN APPLICATION*******************************/
+
+  submit.addEventListener('click', ()=>{
+    // formula
+    // const dayoftheweek = dateborn + monthcode + last2digit + last2digit/4 + centurycode
+    const yearArr = String(year).split("").map((nuyearm)=>{
+      return Number(year)
+    })
+    const last2digit = `${last2digit[2]}${last2digit[3]}`;
+    console.log(last2digit)
+    const monthcode={
+      January : 0, 
+      February : 3 ,
+      March : 3,
+      April : 6,
+      May  : 1,
+      June  :4 ,
+      July :  6,
+      August :  2,
+      September :  5,
+      October :  0,
+      November :  3,
+      December :  5
+    }
+     // century code
+  // 1600 - 1699 => 6
+  // 1700 - 1799 => 6
+  // 1800 - 1899 => 6
+  // 1900 - 1999 => 6
+  // 2000 - 2099 => 6
+ 
+
+
+  // checkbox
+  // let checkbox = document.querySelector('input[type="checkbox"]:checked').value
+
+ 
+
+
+
+
+
   })
  
-  if(window.innerWidth <= 768){
-    sideBar.style.display="none"
-  }
-  else{
-   sideBar.style.display="block"
-  }
- 
- 
-  /**********************************************/
-  // An invalid day should be (d<=0) or (d>31)
-
-  // An invalid month should be (m<= 0) or (m > 12)
- 
-  
- 
-
-
-
-
- const maleNames = [
-  'Kwasi',
-  'Kwadwo',
-  'Kwabena',
-  'Kwaku',
-  'Yaw',
-  'Kofi',
-  'Kwame',
-];
-const femaleNames = [
-  'Akosua',
-  'Adwoa',
-  'Abenaa',
-  'Akua',
-  ' Yaa',
-  'Afua',
-  'Ama',
-];
-// Getting the input values of month, year and day
-submit.addEventListener("click", ()=>{
-  const monthBornIn = month.value;
-  console.log(monthBornIn);
-
-  const yearBornIn = year.value;
-  console.log(yearBornIn);
-
-  const dayBornIn = Day.value;
-  console.log(dayBornIn);
-  const 
-})
-// get the last 2 digit of year value
-
-// get the month value
-
-// check where there century lies i.e 1900 - 1999 => 0 , 2000 - 2099 => 6
-
-// formula
-// (date + monthCode + last2YearDigit + last2YearDigit/4 + centuryCode) % 7.
-
-let gender = 'female';
-let day = 'monday';
-switch (gender) {
-  case 'female':
-    if (day == 'sunday') {
-      console.log(`your akan name is : ${femaleNames[0]}`);
-    }
-    if (day == 'monday') {
-      const resultForMon = `${femaleNames[1]}`;
-       akanName.innerHTML = resultForMon;
-    }
-    if (day == 'tuesday') {
-      const resultForMon = `${femaleNames[1]}`;
-       akanName.innerHTML = resultForMon;    }
-    if (day == 'wednesday') {
-      const resultForMon = `${femaleNames[1]}`;
-       akanName.innerHTML = resultForMon;     }
-    if (day == 'thursday') {
-      const resultForMon = `${femaleNames[1]}`;
-       akanName.innerHTML = resultForMon;    }
-    if (day == 'friday') {
-
-      const resultForMon = `${femaleNames[1]}`;
-       akanName.innerHTML = resultForMon;    }
-    if (day == 'saturday') {
-      const resultForMon = `${femaleNames[1]}`;
-       akanName.innerHTML = resultForMon;    }
-    break;
-
-  case 'male':
-    if ('sunday') {
-      console.log(`day: ${day} => name: ${maleNames[0]}`);
-    }
-    if (day == 'monday') {
-      console.log(`day: ${day} => name: ${maleNames[0]}`);
-    }
-    if (day == 'tuesday') {
-      console.log(`day: ${day} => name: ${maleNames[0]}`);
-    }
-    if (day == 'wednesday') {
-      console.log(`day: ${day} => name: ${maleNames[0]}`);
-    }
-    if (day == 'thursday') {
-      console.log(`day: ${day} => name: ${maleNames[0]}`);
-    }
-    if (day == 'friday') {
-      console.log(`day: ${day} => name: ${maleNames[0]}`);
-    }
-    if (day == 'saturday') {
-      console.log(`day: ${day} => name: ${maleNames[0]}`);
-    }
-    break;
-  default:
-}
-  
-
